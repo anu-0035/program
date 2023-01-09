a380d6e8d29e32669639130ebb87e05fd654e636
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2023-01-09 21:49:31 +0530

    Update 11. leap_year.c

--- 9 Dec 2022/11. leap_year.c
+++ 9 Dec 2022/11. leap_year.c
@@ -5,12 +5,10 @@ int main()
     int year;
     printf("Enter any year to check : ");
     scanf("%d", &year);
-    if((year%400==0)||(year%4==0)){
+    if((year%4==0) and (year%400==0) or (year%100!=0)){
         printf("%d is a leap year \n", year);
     }
-    else if(year%100==0){
-        printf("%d is not a leap year \n", year);
-    }
+    
     else{
         printf("%d is not a leap year \n", year);
     }
