4ede4081a10f1cd51e3d88ae22540a488c9cc101
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2023-01-09 21:54:20 +0530

    Update 6. swap_with_variable.c

--- 2 Dec 2022/6. swap_with_variable.c
+++ 2 Dec 2022/6. swap_with_variable.c
@@ -2,13 +2,11 @@
 #include<stdio.h>
 int main(int argc, char const *argv[])
 {
-    int num1, num2, variable;
+    int num1, num2;
     printf("Enter any two numbers : ");
     scanf("%d %d", &num1, &num2);
     printf("Before swapping : %d %d \n",num1, num2);
-    variable=num1;
-    num1=num2;
-    num2=variable;
+    num1,num2=num2,num1;
     printf("After swapping : %d %d \n", num1, num2);
     return 0;
 }
